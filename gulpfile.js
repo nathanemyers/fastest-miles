7ce9d77c9b6c2e3b772058c75bd028eef83529fc
Nathan Myers <nathanemyers@gmail.com>
2016-08-19 15:46:45 -0500

    adding static assets to gulp compile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ var usemin = require('gulp-usemin');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
 var babel = require('gulp-babel');
+var compass = require('gulp-compass');
 
-gulp.task('build', function() {
+gulp.task('build', ['moveData', 'moveAssets', 'compileSass'], function() {
   return gulp.src(['index.html'])
     .pipe(usemin({
       css: [minifyCss()],
@@ -15,7 +16,21 @@ gulp.task('build', function() {
   .pipe(gulp.dest('dist/'));
 });
 
+gulp.task('compileSass', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(compass({
+      config_file: './config.rb'
+    }))
+    .pipe(gulp.dest('stylesheets/'));
+});
+
 gulp.task('moveData', function() {
   return gulp.src('data/*.csv')
     .pipe(gulp.dest('dist/data/'));
 });
+
+gulp.task('moveAssets', function() {
+  return gulp.src('assets/*.svg')
+    .pipe(gulp.dest('dist/assets/'));
+});
+
