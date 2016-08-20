5a04bcd2fb79554d789f23cd3513453616521e04
Nathan Myers <nathanemyers@gmail.com>
2016-08-19 20:06:46 -0500

    adding some deploy logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,45 @@
+var fs = require('fs');
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var usemin = require('gulp-usemin');
 var minifyCss = require('gulp-minify-css');
 var rev = require('gulp-rev');
 var babel = require('gulp-babel');
+var polyfill = require('babel-polyfill');
 var compass = require('gulp-compass');
 var replace = require('gulp-replace');
+var scp = require('gulp-scp2');
 
-var dataUrl = 'foobar';
-var assetUrl = 'foobar';
+var secret = JSON.parse(fs.readFileSync('secret.json', 'utf8'));
+
+var dataUrl = secret.dataUrl;
+var assetUrl = secret.assetUrl;
+
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('dist/')
+    .pipe(scp({
+      host: secret.server,
+      username: secret.username,
+      password: secret.password,
+      dest: secret.serverDestination
+    }))
+    .on('error', function(err) {
+      console.log(err);
+    });
+});
 
 gulp.task('build', ['moveData', 'moveAssets', 'compileSass'], function() {
   return gulp.src(['src/index.html'])
     .pipe(usemin({
       css: [minifyCss()],
       libjs: [uglify()],
-      js: [replace('\.\.\/data', dataUrl), replace('\.\.\/assets', assetUrl), babel({presets: 'es2015'}), uglify()]
+      js: [
+        replace('\.\.\/data', dataUrl), 
+        replace('\.\.\/assets', assetUrl), 
+        //babel({presets: 'es2015'}), 
+        //uglify()
+      ]
     }))
   .pipe(gulp.dest('dist/'));
 });
